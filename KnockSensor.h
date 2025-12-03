83ac1b6a2eca8b969be4a914f077fcc2d16e91b3
Pache <54517063+Pachieeee@users.noreply.github.com>
2025-12-03 17:58:37 -0500

    feat(sensor): update KnockSensor constructor to allow optional event handler.

--- KnockSensor.h
+++ KnockSensor.h
@@ -8,7 +8,7 @@ public:
     static const int KNOCK_DETECTED_EVENT_ID = 10;
     static const Event KNOCK_DETECTED_EVENT;
 
-    KnockSensor(int pin, EventHandler* eventHandler);
+    KnockSensor(int pin, EventHandler* eventHandler = nullptr);
 
     void scanKnock();
 };
